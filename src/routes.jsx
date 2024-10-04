ab57f6f4fb4d268abef03b2c67c725afd749b028
Daniel Lopez <danlopez80504@gmail.com>
2024-10-04 16:58:38 -0500

    move around some stuff and added initial folders

---
+++ src/routes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useContext, useEffect } from "react";
+import { Route, Routes, useRoutes } from "react-router-dom";
+import { Session } from "../../server/trpc/auth/auth";
+import { AuthContext, AuthProvider } from "./auth/SessionProvider";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Signup from "./pages/Signup";
+
+const baseRoutes = [
+    { path: "/", index: true, element: <Home />, label: "Home", isNav: true },
+    { path: "signup", element: <Signup />, label: "Signup", isNav: false },
+    { path: "login", element: <Login />, label: "Login", isNav: false },
+    { path: "profile", element: <Profile />, label: "Profile", isNav: false },
+    { path: "store", element: <Products />, label: "Store", isNav: true },
+  ];
