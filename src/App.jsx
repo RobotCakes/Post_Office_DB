e3050e5f5c03ed10e1a76d6e569316b908823256
Daniel Lopez <danlopez80504@gmail.com>
2024-10-16 19:07:12 -0500

    register page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 // testing desktop import { useAuth } from "./auth/SessionProvider"; make session provider for authentication? ~davis 
-
 import reactLogo from './assets/react.svg'
 import './styles/test.css'
 import Signup from "./pages/Signup";
