9c4e2cd55df549766b38ade2410dde287ee92af4
Daniel Lopez <danlopez80504@gmail.com>
2024-10-16 19:14:47 -0500

    Register page created

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -114,7 +114,7 @@ const Signup = () => {
                     <FontAwesomeIcon icon={faInfoCircle} />
                     8 to 24 characters.<br />
                     Must include uppercase and lowercase letters, a number and a special character.<br />
-                    Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                    Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="dollar sign">$</span> 
                 </p>
 
                 <label htmlFor="confirmPwd">
