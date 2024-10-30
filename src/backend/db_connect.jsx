3035f680bfd818ec6d6a4b4812d3414cc2c71023
Daniel Lopez <danlopez80504@gmail.com>
2024-10-30 16:48:35 -0500

    created most of the report queries and changed some stuff for db_connect.

--- src/backend/db_connect.jsx
+++ src/backend/db_connect.jsx
@@ -1,20 +1,25 @@
+//Connecting to DB
 const sql = require('mssql');
 const config = {
-    user: 'alejandro',
-    password: 'Michaeljordan1990@',
+    user: 'ashley',
+    password: 'Jameharden4000@',
     server: 'ohyeahmrpostman2.database.windows.net',
     database: 'group10',
     port: 1433,
     options: {
-        encrypt: true
+        encrypt: true,
+        trustServerCertificate: false
     }
 };
 
-var conn = new sql.ConnectionPool(config);
+const pool = new sql.ConnectionPool(config);
 
-sql.connect(config)
-    .then(() => console.log('Connected to database'))
-    .catch(err => console.error('Failed to connect to database', err));
+pool.connect()
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch(err => {
+        console.error('Failed to connect to database', err);
+    });
 
-//exports.conn = conn;
-exports.config = config;
+
