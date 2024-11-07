d446cd52dd04d8e25f8cc1e48c7322691ce42ee7
ddouuu <davisouuu@gmail.com>
2024-11-07 15:29:37 -0600

    updating manageTruck.jsx

--- src/pages/employee/manageTruck.jsx
+++ src/pages/employee/manageTruck.jsx
@@ -1,12 +1,9 @@
 import { useRef, useState, useEffect } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { Link, Routes, Route, useMatch, useResolvedPath } from "react-router-dom"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-//import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { /*Link,*/ useNavigate } from 'react-router-dom';
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";  // Single import for icons
+import { Link, Routes, Route, useMatch, useResolvedPath, useNavigate } from "react-router-dom";  // Combine all react-router-dom imports into one line
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";  // FontAwesomeIcon import remains the same
 
 const manageTruck = () => {
+};
 
-
-}
 export default manageTruck;
